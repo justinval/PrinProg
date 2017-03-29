990cd9f21edecf7baf92e40453466c60574b5bf2
imjustinv <imjustinv@gmail.com>
2017-03-29 12:27:33 -0400

    Fixed assign()

--- Compiler.c
+++ Compiler.c
@@ -175,7 +175,7 @@ static void assign()
 		if (token == '=') {
 			next_token();
 			int a = expr();
-			CodeGen(STOREAI, a, 0, (token-'a')*4);
+			CodeGen(STOREAI, 0, a, (token-'a')*4);
 		}
 	} else {
 		ERROR("Expected '='\n");
